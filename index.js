2951974c181653351b56b64354c6ebac481f8dde
buildergirl <aprilruthowens@gmail.com>
2018-07-12 20:25:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+
+var driver = {
+  name: "Sam",
+  address: "11 Broadway"
+};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+const copyDriver = 
+Object.assign({}, driver, {[key]: value});
+return copyDriver
+}
